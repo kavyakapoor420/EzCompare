08727ae66cb30a213e3f7659646da36c555c620f
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-07-03 00:09:16 +0530

    completed code for instamart scrapping using puppeteer

---
+++ NodeBackend/Scrapers/InstamartScrapper.js
@@ -0,0 +1,101 @@
+const puppeteer=require('puppeteer')
+
+const delay=ms=> new Promise(r=>setTimeout(r,ms))
+
+//swiigy -> instamart 
+async function swiggyScrape(query,location){
+    
+    const {latitude,longitude}=location 
+
+    const browser=await puppetter.lauch({
+        headless:true,
+        args:['--no-sandbox', '--disable-setuid-sandbox','--use-fake-ui-for-media-stream',],
+        defaultViewport:{width:1280 ,height:800}
+    })
+
+    const context=browser.defaultBrowserContext() 
+    await context.overridePermissions("https://www.swiggy.com", ["geolocation"]);
+    const page=await browser.newPage() ;
+
+    await page.setGeolocation({latitude:latitude,longitude:longitude})
+    
+    try{
+         await page.goto('https://www.swiggy.com/instamart',{ waitUntil: 'networkidle2' });
+         await page.waitForSelector('[data-testid="set-gps-button"]', { visible: true });
+         await page.click('[data-testid="set-gps-button"]')
+
+    }catch(err){
+         await delay(500)  ;// give time for tooltip to appear
+         await page.waitForFunction(()=>{
+            const tooltip=document.querySelector('[data-testid="re-check-address-tooltip"]');
+            const closeBtn=tooltip?.querySelector('[role="button"]')
+            return !!closeBtn
+         })
+
+     await page.evaluate(()=>{
+        const tooltip=document.querySelector('[data-testid="re-check-address-tooltip"]');
+        const closeBtn=tooltip?.querySelector('[role="button"]')
+        closeBtn?.click()
+     })
+
+        await page.waitForSelector('div._1AaZg',{visible:true});
+        await page.click('div._1AaZg')
+        await delay(200)
+
+        await page.waitForSelector('[data-testid="search-page-header-search-bar-input"]', { visible: true });
+        await page.type('[data-testid="search-page-header-search-bar-input"]', query);
+        await page.keyboard.press("Enter")
+
+        await page.waitForSelector('[data-testid="IM_SEARCH_PAGE_TITLE"]',{visible:true});
+        await page.evaluate(()=>{
+            window.scrollBy(0,window.innerHeight) ; // small scroll to triggeer image laod 
+        })
+        await page.evaluate(async()=>{
+            const items=Array.from(document.querySelectorAll('[data-testid="default_container_ux4"]'))
+            for(const item of items){
+                item.scrollIntoView({behavior:'instant',block:'center'})
+                await new Promise(resolve=> setTimeout(resolve,100)) ; //  let lazy image load
+            }
+        })
+
+        await delay(200)
+        await page.setGeolocation({latitude,longitude})
+
+        const products=await page.evaluate(()=>{
+            function generateSwiggySearchURL(productName) {
+                const encodedQuery=encodeURIComponent(productName).replace(/%20/g,'+')
+                return `https://www.swiggy.com/instamart/search?custom_back=true&query=${encodedQuery}`;
+            }
+
+            const items=Array.from(document.querySelectorAll('[data-testid="default_container_ux4"]'))
+
+
+            return items.map(item=>{
+                const name=item.querySelector('.novMV')?.innerText || ''
+                const productImg=Array.from(item.querySelectorAll('img'))
+                    .map(img=>img.src)
+                    .find(src => src.includes('media-assets.swiggy.com') && !src.includes('instamart-media-assets')) || null;
+
+                const quantity=item.querySelector('FqnWn')?.innerText ||  ""
+                const deliveryLabel=item.querySelector('[aria-label^="Delivery in"]')?.getAttribute('aria-label') || '';
+                const deliveryTime=deliveryLabel.match(/\d+/)?.[0] || ''
+                const price=item.querySelector('[data-testid="item-offer-price"]')?.innerText || '';
+                const link=generateSwiggySearchURL(name)
+                const isSoldOut=!!item.querySelector('[data-testid="sold-out"]');
+                const availability=isSoldOut ? 'Sold Out' : 'Available' 
+
+
+                return {name,productImg,quantity,deliveryTime,price,link,availability}
+            })
+
+        })
+
+       await browser.close() 
+
+       return products
+    }
+}
+
+module.exports=swiggyScrape
+
+
--- NodeBackend/app.js
+++ NodeBackend/app.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const cors=require('cors')
+const { fetchLocation } = require('./ApiFunction/fetchLocation')
 
 
 const app=express() 
@@ -9,6 +10,31 @@ app.use(express.json())
 
 app.post('/search/swiggy',async(req,res)=>{
     const {query,pincode}=req.body ;
+    const location=await fetchLocation(pincode)
+
+    if(!query || !pincode){
+        return res.status(400).json({error:'query and pincode required'})
+    }
+    if(!location || !location.latitude || !location.longitude){
+        return res.status(400).json({error:'try searching with  a more specific locality name '})
+    }
+    try{
+        const encodedQuery=encodeURIComponent(query)
+        const swiggyUrl=`https://www.swiggy.com/search?location=${location.latitude},${location.longitude}&q=${encodedQuery}`
+        
+        // Fetching the Swiggy search results page
+        const response=await fetch(swiggyUrl)
+        if(!response.ok){
+            throw new Error('Failed to fetch data from Swiggy')
+        }
+        
+        const html=await response.text()
+        console.log('Swiggy search results fetched successfully')
+        return res.status(200).json({html})
+    }catch(err){
+        console.error('error scraaping swiggy',err)
+        return res.status(500).json({error:'failed to fetch data'})
+    }
 })
 
 app.listen(3000,()=>{
